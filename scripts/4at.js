5296b9f26747502bb2859b793f268e1e5035f85b
vamonospest <61283777+vamonospest@users.noreply.github.com>
2020-04-06 18:30:09 +0300

    update name and localStorage

--- scripts/4at.js
+++ scripts/4at.js
@@ -1,6 +1,8 @@
 //dom queries
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
 
 //add a new chat
 newChatForm.addEventListener('submit', e => {
@@ -11,9 +13,25 @@ newChatForm.addEventListener('submit', e => {
         .catch(err => console.log(err));
 });
 
+//update username
+newNameForm.addEventListener('submit', e => {
+    e.preventDefault();
+    //update name via .chatroom
+    const newName = newNameForm.name.value.trim();
+    chatroom.updateName(newName);
+    //reset form
+    newNameForm.reset();
+    //show and hide update mssg
+    updateMssg.innerText = `Вы изменили своё имя на ${newName}`;
+    setTimeout(() => updateMssg.innerText = '', 3000);
+});
+
+//check localStorage for name
+const username = localStorage.username ? localStorage.username : 'anon';
+
 //class patterns
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('gaming', 'shaun');
+const chatroom = new Chatroom('gaming', username);
 
 //get chats and render
 chatroom.getChats(data => chatUI.render(data));
