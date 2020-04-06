1962bd169a6aaa6fbd4278a88ddf9ed5e6c8b294
vamonospest <61283777+vamonospest@users.noreply.github.com>
2020-04-06 17:37:25 +0300

    Update 4at.js

--- scripts/4at.js
+++ scripts/4at.js
@@ -1,1 +1,9 @@
-//4at.js comment
+//dom queries
+const chatList = document.querySelector('.chat-list');
+
+//class patterns
+const chatUI = new ChatUI(chatList);
+const chatroom = new Chatroom('gaming', 'shaun');
+
+//get chats and render
+chatroom.getChats(data => chatUI.render(data));
